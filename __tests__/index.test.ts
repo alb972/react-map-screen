bc98e9c60ccefdd736ea3f4aea2ba9daf178eccc
DUFRENOT Andy <andy.dufrenot@docdoku.com>
2023-01-28 15:34:28 +0100

    [Feat] Improves filter & adds tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,16 +1,46 @@
 import * as React from "react";
+import * as Utils from "../src/utils/methods";
+import { RestaurantDTO } from "../src/model/RestaurantDTO";
 
-describe("Hello", () => {
-  it("should return hello world", () => {
-    const result = "Hello World!";
-    expect(result).toEqual("Hello World!");
-  });
-});
+describe("Filters", () => {
+  describe("getRestaurantsFilteredByName", function () {
+    it("should return an empty list", () => {
+      const restaurants: Array<RestaurantDTO> = [
+        {
+          businessName: "Pizzeria",
+          latitude: null,
+          longitude: null,
+          simplifiedMarketSegment: null,
+        },
+      ];
+      const result = Utils.getRestaurantsFilteredByName(restaurants, "le chill");
+      expect(result.length).toEqual(0);
+    });
 
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      expect([1, 2, 3].indexOf(3)).toEqual(2);
+    it("should return a match when complete name passed", () => {
+      const restaurants: Array<RestaurantDTO> = [
+        {
+          businessName: "LE CHILL CLUB",
+          latitude: null,
+          longitude: null,
+          simplifiedMarketSegment: null,
+        },
+      ];
+      const result = Utils.getRestaurantsFilteredByName(restaurants, "le chill club");
+      expect(result.length).toEqual(1);
+    });
+
+    it("should return a match when complete name passed (uppercase)", () => {
+      const restaurants: Array<RestaurantDTO> = [
+        {
+          businessName: "LE CHILL CLUB",
+          latitude: null,
+          longitude: null,
+          simplifiedMarketSegment: null,
+        },
+      ];
+      const result = Utils.getRestaurantsFilteredByName(restaurants, "LE CHILL CLUB");
+      expect(result.length).toEqual(1);
     });
   });
 });
